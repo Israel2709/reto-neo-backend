85954185084a1ab08d4d62501987ededf652b50a
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-08-13 09:29:25 -0500

    added cors module

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,87 +5,95 @@ const router = express.Router()
 const users = require('../usecases/users')
 
 
-router.get('/', async( request, response) =>{
- try {
-   const allUsers = await users.getAll()
-   response.status(200)
-   response.json({
-     succes:true,
-     data:{
-       users: allUsers
-     }
-   })   
- } catch (error) {
-   response.status(400)
-   response.json({
-     succes: false,
-     error: error.message
-   })   
- }
+router.get('/', async (request, response) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
+    const allUsers = await users.getAll()
+    response.status(200)
+    response.json({
+      succes: true,
+      data: {
+        users: allUsers
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      succes: false,
+      error: error.message
+    })
+  }
 })
 
 
-router.post('/', async (request,response) =>{ 
- try {  
+router.post('/', async (request, response) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
     const newUserData = request.body
     const newUser = await users.createUser(newUserData)
     //response.status(200)
     response.json({
-      succes:true,
-      data:{
+      succes: true,
+      data: {
         newUser
       }
     })
- } catch (error) {  
-     // response.status(400)
-     response.json({
-       succes:false,
-       error:error.message
-     })
-   
- }
+  } catch (error) {
+    // response.status(400)
+    response.json({
+      succes: false,
+      error: error.message
+    })
+
+  }
 })
 
-router.patch('/:id', async(request,response) =>{
-try {
-  const newUserData = request.body
-  const idUser = request.params.id
-  const newUSer = await users.updateUser(idUser, newUserData)
-  response.status(200)
-  response.json({
-    succes:true,
-    data:{
-      newUSer
-    }
-  })
-  
-} catch (error) {
-  response.status(400)
-  response.json({
-    succes:false,
-    error: error.message
-  })
-}
+router.patch('/:id', async (request, response) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
+    const newUserData = request.body
+    const idUser = request.params.id
+    const newUSer = await users.updateUser(idUser, newUserData)
+    response.status(200)
+    response.json({
+      succes: true,
+      data: {
+        newUSer
+      }
+    })
+
+  } catch (error) {
+    response.status(400)
+    response.json({
+      succes: false,
+      error: error.message
+    })
+  }
 })
 
-router.delete('/:id', async(request,response) =>{
- try {
-   const deleteUserId = request.params.id
-   const userDelete = await users.deleteUser(deleteUserId)
-   response.status(200)
-   response.json({
-     succes: true,
-     data:{
-       userDelete
-     }
-   })
- } catch (error) {
-   response.status(400)
-   response.json({
-     succes:false,
-    error: error.message
-   })
- }
+router.delete('/:id', async (request, response) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  try {
+    const deleteUserId = request.params.id
+    const userDelete = await users.deleteUser(deleteUserId)
+    response.status(200)
+    response.json({
+      succes: true,
+      data: {
+        userDelete
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      succes: false,
+      error: error.message
+    })
+  }
 })
 
 module.exports = router
