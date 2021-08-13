a0c811b61e996accccb57bb6f4cf23fdc598f32b
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-08-13 04:57:27 -0500

    port setup

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 
 const dbConnect = require('./src/lib/db')
 const server = require ('./src/server')
+const port = 8080 || 3000
 
 dbConnect()
 .then(() =>{
   console.log('DB connected')
-  server.listen(8080, () =>{
-    console.log('server is listening')
+  server.listen(port, () =>{
+    console.log(`server is listening in port ${port}`)
   })  
 }).catch(error =>{
   console.error('Error',error)
